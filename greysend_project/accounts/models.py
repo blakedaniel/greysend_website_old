9d647e9dd8eaa8ff9a3fc6f1733d0844a980b274
Abraham Paniagua <abraham.paniagua@gmail.com>
2023-04-18 09:37:02 -0700

    Added ChatUser model in Account

--- greysend_project/accounts/models.py
+++ greysend_project/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class ChatUser(models.Model):
+    given_name = models.CharField(max_length=50)
+    family_name = models.CharField(max_length=50)
+    user_name = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user_name
