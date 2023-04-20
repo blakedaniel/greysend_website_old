50b4b1a01df1aba841500fa1e0e62f267ab4671f
Abraham Paniagua <abraham.paniagua@gmail.com>
2023-04-20 08:45:13 -0700

    Added accounts/members page

--- greysend_project/accounts/urls.py
+++ greysend_project/accounts/urls.py
@@ -2,5 +2,6 @@ from accounts import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('members', views.list_users, name='index')
 ]
